271dd364ebbbf07d0a4041a9312d67d49c841786
amank-27 <amankhan19989270@gmail.com>
2025-03-12 22:10:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [coupon, setCoupon] = useState('');
+
+  const claimCoupon = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/claim-coupon');
+      setMessage(response.data.message);
+      setCoupon(response.data.coupon_code);
+    } catch (error) {
+      if (error.response) {
+        setMessage(error.response.data.message);
+      } else {
+        setMessage("An error occurred. Please try again later.");
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Coupon Distribution</h1>
+      <button onClick={claimCoupon}>Claim Coupon</button>
+      <p>{message}</p>
+      {coupon && <p>Your coupon code is: {coupon}</p>}
+    </div>
+  );
+}
+
+export default App;
